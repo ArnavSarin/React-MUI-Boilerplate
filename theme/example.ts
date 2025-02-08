d5f421bec88f951795a76a7d9815f7025f7e1324
Arnav Sarin <arnavsarin@Arnav-s-Macbook-16--1-408-805-9703.local>
2025-02-08 17:45:50 -0600

    Themeing

--- theme/example.ts
+++ theme/example.ts
@@ -1,4 +1,4 @@
-import { Poppins, Roboto } from 'next/font/google';
+import { Poppins } from 'next/font/google';
 import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
